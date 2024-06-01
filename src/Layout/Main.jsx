b6806558ccfb204b0f153e8842a3c65eb698331c
Fazlul Alam <fazlulalam619@gmail.com>
2024-06-02 00:43:56 +0600

    Nav bar And Footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import FooterPage from "../Pages/Shared/Footer/FooterPage";
+import NavBar from "../Pages/Shared/Nav Bar/NavBar";
 const Main = () => {
   return (
     <div>
+      <NavBar></NavBar>
       <Outlet></Outlet>
+      <FooterPage></FooterPage>
     </div>
   );
 };

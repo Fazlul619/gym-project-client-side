7903474659cc93c1bec240295ef7f3348aaa0dee
Fazlul Alam <fazlulalam619@gmail.com>
2024-06-10 03:30:03 +0600

    rearranged all trainers and functionalities to delete a trainer(changing their role to member)

--- src/Pages/Dash Board/All Trainers/AllTrainers.jsx
+++ src/Pages/Dash Board/All Trainers/AllTrainers.jsx
@@ -1,87 +1,102 @@
+import { useQuery } from "@tanstack/react-query";
 import Modal from "../../../components/Modal";
-import useAllTrainer from "../../../hooks/useAllTrainer";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useModal from "../../../hooks/useModal";
+import Swal from "sweetalert2";
 
 const AllTrainers = () => {
-  const [allTrainers] = useAllTrainer();
+  const axiosSecure = useAxiosSecure();
+  const { data: users = [], refetch } = useQuery({
+    queryKey: ["allUsers"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/allUsers");
+      return res.data;
+    },
+  });
 
   const { isOpen, openModal, closeModal } = useModal();
 
+  const usersWhoAreTrainer = users.filter((user) => user.role === "trainer");
+
+  const handleDeleteTrainer = (trainer) => {
+    axiosSecure.patch(`/users/${trainer._id}`).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${trainer.name} is a Member Now!`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
+
   return (
     <div className="flex flex-col gap-5">
-      <div className="flex list-none underline">
-        <div className="flex items-center w-full flex-[0.5] gap-5 text-xl font-bold">
-          <li>Image</li>
-          <li>Name</li>
+      <div className="flex justify-between underline list-none">
+        <div className="w-1/3">
+          <li className="text-xl font-bold">Name</li>
         </div>
-        <div className="flex-[1.5] w-full flex justify-between text-xl font-bold">
-          <li className="flex-1 text-left">Email</li>
-          <li className="flex-1 text-left">Age</li>
-          <li className="flex-1 text-left">Experience</li>
-          <li className="flex-1 text-right">Delete</li>
+        <div className="w-1/3">
+          <li className="text-xl font-bold">Email</li>
+        </div>
+        <div className="w-1/3 text-right">
+          <li className="text-xl font-bold">Delete</li>
         </div>
       </div>
-      {allTrainers.map((trainer) => {
-        return (
-          <li key={trainer._id} className="flex items-center w-full list-none">
-            <div className="flex items-center gap-5 flex-[0.5]">
-              <img
-                className="w-16 h-16 rounded-full object-cover"
-                src={trainer.profileImage}
-                alt="trainer's profile pic"
-              />
+      {usersWhoAreTrainer.map((trainer) => (
+        <div
+          key={trainer._id}
+          className="flex justify-between items-center w-full"
+        >
+          <div className="w-1/3">
+            <h2 className="text-xl font-semibold">{trainer.name}</h2>
+          </div>
+          <div className="w-1/3">
+            <p>{trainer.email}</p>
+          </div>
+          <div className="w-1/3 text-right">
+            <button
+              className="bg-rose-500/70 px-3 py-1 rounded-sm"
+              onClick={openModal}
+            >
+              Delete
+            </button>
 
-              <h2 className="text-xl font-semibold">{trainer.name}</h2>
-            </div>
-
-            <div className="flex-[1.5] w-full flex justify-between text-base font-semibold">
-              <p className="flex-1 text-left">{trainer.email}</p>
-              <p className="flex-1 text-left">{trainer.age} Years old</p>
-              <p className="flex-1 text-left">
-                {`${trainer.experience} ${
-                  parseInt(trainer.experience, 10) > 1 ? "Years" : "Year"
-                }`}{" "}
-              </p>
-              <div className="w-full flex-1 text-right">
-                <button
-                  className="bg-rose-500/70 px-3 py-1 rounded-sm"
-                  onClick={openModal}
-                >
-                  Delete
-                </button>
-
-                <Modal isOpen={isOpen} onClose={closeModal}>
-                  <div className="flex gap-5 flex-col">
-                    <div>
-                      <h1 className="text-2xl font-semibold font-mono">
-                        Are you sure?
-                      </h1>
-                      <p className="text-xs">
-                        Deleting a trainer will change their role to member.
-                      </p>
-                    </div>
-
-                    <div className="flex gap-3 justify-end min-w-fit text-lg">
-                      <button
-                        onClick={() => {}}
-                        className="px-3 py-1 bg-rose-400 rounded-sm"
-                      >
-                        Delete
-                      </button>
-                      <button
-                        className="px-3 py-1 bg-cyan-400 rounded-sm"
-                        onClick={closeModal}
-                      >
-                        Cancel
-                      </button>
-                    </div>
+            {isOpen && (
+              <Modal isOpen={isOpen} onClose={closeModal}>
+                <div className="flex flex-col gap-5">
+                  <div>
+                    <h1 className="text-2xl font-semibold font-mono">
+                      Are you sure?
+                    </h1>
+                    <p className="text-xs">
+                      Deleting a trainer will change their role to member.
+                    </p>
                   </div>
-                </Modal>
-              </div>
-            </div>
-          </li>
-        );
-      })}
+                  <div className="flex justify-end gap-3 min-w-fit text-lg">
+                    <button
+                      onClick={() => handleDeleteTrainer(trainer)}
+                      className="px-3 py-1 bg-rose-400 rounded-sm"
+                    >
+                      Delete
+                    </button>
+                    <button
+                      className="px-3 py-1 bg-cyan-400 rounded-sm"
+                      onClick={closeModal}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </Modal>
+            )}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
